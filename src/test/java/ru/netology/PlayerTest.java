6823b5b8a375d91c3a24392b292b3b153e7b87a2
Aleksandr Kretov <kretov_a@outlook.com>
2022-03-30 23:39:30 +0300

    скопировал с github'a test'ы и исправил метод play

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
 
-    @Test
+
+    @Test // Суммирует время, проигранныые во все игры жанра "Аркады" игроком "Петя", если добавлена ТОЛЬКО ОДНА ИГРА
     public void shouldSumGenreIfOneGame() {
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
@@ -19,5 +20,58 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
 
-    // другие ваши тесты
+    @Test // Суммирует время, проигранныые во все игры жанра "Аркады" игроком "Петя", если добавлено НЕСКОЛЬКО ИГР
+    public void shouldSumGenreIfSomeGame() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Покорение Марса", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.play(game1, 3);
+
+        player.installGame(game2);
+        player.play(game2, 5);
+
+        int expected = 8;
+        int actual = player.sumGenre(game1.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    @Test // Считает количество часов, которое играли в игру, если она УСТАНОВЛЕНА
+    public void shouldCountHoursWhenGameInstalled() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+
+        int actual = player.play(game, 3);;
+        assertEquals(3, actual);
+    }
+
+    @Test // Выкидывает исключение пр ипопытке сосчитать количество часов, которое играли в игру, если она НЕ УСТАНОВЛЕНА
+    public void shouldThrowExceptionWhenGameNotInstalled() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+
+        assertThrows(RuntimeException.class, () -> player.play(game, 3));
+    }
+
+    @Test // Выдает игру заданного жанра "Аркады", в которую играли бОльшее количество часов
+    public void shouldReturnMostPlayedByGenre() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Покорение Марса", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.play(game1, 3);
+
+        player.installGame(game2);
+        player.play(game2, 5);
+
+        assertEquals("Покорение Марса", player.mostPlayerByGenre("Аркады"));    }
 }

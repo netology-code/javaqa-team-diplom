09751fd4d1c02dc5e194f246203db95801393369
Aleksandr Kretov <kretov_a@outlook.com>
2022-03-30 17:07:29 +0300

    Вроде все тесты какие в голову пришли.

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -3,6 +3,9 @@ package ru.netology;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameStoreTest {
 
     @Test
@@ -13,6 +16,54 @@ public class GameStoreTest {
 
         assertTrue(store.containsGame(game));
     }
+
+    @Test
+    public void shouldAddPlayerTime(){
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        store.addPlayTime("Vasiliy", 3);
+        assertEquals(store.getSumPlayedTime(), 3);
+    }
+ /*Тест на самого играющего*/
+    @Test
+    public void shouldMostPlayerTime(){
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        store.addPlayTime("Vasiliy", 5);
+        store.addPlayTime("Genadiy", 2);
+        assertEquals(store.getMostPlayer(),"Vasiliy");
+
+    }
+    /*Тест на самого играющего при одинкаовых значениях*/
+    @Test
+    public void shouldMostPlayerTimeSameValues(){
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        store.addPlayTime("Vasiliy", 5);
+        store.addPlayTime("Genadiy", 5);
+        assertEquals(store.getMostPlayer(),"Vasiliy");
+
+    }
+/*Тест на суммирование метода addPlayTime*/
+    @Test
+    public void shouldSummPlayerTime(){
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        store.addPlayTime("Genadiy", 4);
+        store.addPlayTime("Vasiliy", 5);
+        store.addPlayTime("Genadiy", 2);
+        assertEquals(store.getMostPlayer(),"Genadiy");
+
+    }
+
+    /*Ищет самого играющего игрока при отсутствии их*/
+    @Test
+    public void shouldGetMostPlayerWithNull(){
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        assertEquals(store.getMostPlayer(), null);
+    }
+
 
     // другие ваши тесты
 }

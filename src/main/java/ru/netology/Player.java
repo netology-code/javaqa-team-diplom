a252a38dd276334b50d66af9c4d3befc01db93f4
DesertScream <www.anton.dzuin@gmail.com>
2022-04-11 14:11:28 +0500

    bugfix in the mostPlayerByGenre method #6

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -57,6 +57,17 @@ public class Player {
     /** Метод принимает жанр и возвращает игру этого жанра, в которую играли больше всего
      Если в игры этого жанра не играли, возвращается null */
     public Game mostPlayerByGenre(String genre) {
-        return null;
+        int maxTime = 0;
+        Game maxGame = null;
+        for (Game game : playedTime.keySet()) {
+            if (game.getGenre().equals(genre)) {
+                int playerTime = playedTime.get(game);
+                if (playerTime > maxTime) {
+                    maxTime = playerTime;
+                    maxGame = game;
+                }
+            }
+        }
+        return maxGame;
     }
 }
